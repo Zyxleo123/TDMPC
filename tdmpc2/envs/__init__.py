899760fd909622ea749348bd9e0e1dfe17ef45e7
Yixi Zhao <yixiz@andrew.cmu.edu>
2026-03-20 21:33:49 -0400

    can specify seed_steps

--- tdmpc2/envs/__init__.py
+++ tdmpc2/envs/__init__.py
@@ -85,7 +85,8 @@ def make_env(cfg):
             cfg.obs_shape = {cfg.get("obs", "state"): env.observation_space.shape}
             cfg.action_dim = env.action_space.shape[0]
             cfg.episode_length = env.max_episode_steps
-            cfg.seed_steps = max(1000, 5 * cfg.episode_length)
+            if cfg.seed_steps is None:
+                cfg.seed_steps = max(1000, 5 * cfg.episode_length)
             return env
 
         env = None
